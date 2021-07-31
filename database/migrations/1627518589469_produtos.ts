ca4df755e544e997d56d4cf72e2852cbf2b115f3
Jéssica Leal <62772815+Jessy777-cripto@users.noreply.github.com>
2021-07-30 23:03:06 -0300

    fix(model): renomeando colunas da tabela produtos

--- database/migrations/1627518589469_produtos.ts
+++ database/migrations/1627518589469_produtos.ts
@@ -6,17 +6,14 @@ export default class Produtos extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('nomeProduto').notNullable()
+      table.string('nome_produto').notNullable()
       table.text('ingredientes')
-      table.decimal('preco').unsigned()
-
-      table.integer('categoriaId').unsigned().references('categoria.id').onDelete('CASCADE')
-
+      table.decimal('preco').unsigned().notNullable()
+      table.integer('categoria_id').unsigned().references('categorias.id').onDelete('CASCADE')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
   }
-
   public async down () {
     this.schema.dropTable(this.tableName)
   }
