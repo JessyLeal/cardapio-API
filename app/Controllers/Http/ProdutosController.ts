99ff48a7b84c68ced7eb69346ee0caeafc6e2cae
Jéssica Leal <62772815+Jessy777-cripto@users.noreply.github.com>
2021-07-30 23:08:06 -0300

    fix(controller): correção e aperfeiçoamento do CRUD
    - O onDelete('Cascade') não funcionava, logo alterei o destrutor para deletar todos os produtos com categoria_id igual ao da categoria deletada por meio da query.
    - Alteração de variáveis
    - Inserção do .merge para alterar somente o atributo desejado.
    - Inserção da constante 'ifexist' para prevenir que produtos sejam inseridos em categorias inexistentes.
    - Correção de erros

--- app/Controllers/Http/ProdutosController.ts
+++ app/Controllers/Http/ProdutosController.ts
@@ -1,21 +1,26 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Produto from 'App/Models/Produto'
+import Categoria from 'App/Models/Categoria'
 
 export default class ProdutosController {
   public async index ({}: HttpContextContract) {
     return Produto.all()
   }
 
-  public async store ({request}: HttpContextContract) {
-    const {categoriaId}= request.params()
-    const {nomeProduto, ingredientes, preco} = request.body()
-    const pdt = await Produto.create({nomeProduto, ingredientes, preco, categoriaId})
+  public async store ({request,response}: HttpContextContract) {
+    const {categoria_id}= request.params()
+    const {nome_produto, ingredientes, preco} = request.body()
+    const ifexist = await Categoria.findBy('id', categoria_id)
+    if (!ifexist){
+      return response.notFound()
+    }
+    const pdt = await Produto.create({nomeProduto:nome_produto, ingredientes, preco, categoriaId:categoria_id})
     return pdt
   }
 
   public async show ({request, response}: HttpContextContract) {
-    const{categoriaId, id} = request.params()
-    const pdt = await Produto.query().where('id',id).andWhere('categoriaId', categoriaId).first()
+    const{categoria_id, id} = request.params()
+    const pdt = await Produto.query().where('id',id).andWhere('categoria_id', categoria_id).first()
     if (!pdt){
       return response.notFound()
     }
@@ -23,22 +28,25 @@ export default class ProdutosController {
   }
 
   public async update ({request, response}: HttpContextContract) {
-    const {categoriaId,id} = request.params()
-    const {nomeProduto, ingredientes, preco} = request.body()
-    const pdt = await Produto.query().where('id',id).andWhere('categoriaId', categoriaId).first()
+    const {categoria_id,id} = request.params()
+    const {nome_produto, ingredientes, preco} = request.body()
+    const pdt = await Produto.query().where('id',id).andWhere('categoria_id', categoria_id).first()
     if (!pdt){
       return response.notFound()
     }
-    pdt.nomeProduto = nomeProduto
-    pdt.ingredientes = ingredientes
-    pdt.preco= preco
-    await pdt.save()
+    await pdt
+    .merge({
+      nomeProduto: nome_produto,
+      ingredientes: ingredientes,
+      preco: preco
+    })
+    .save()
     return pdt
   }
 
   public async destroy ({request, response}: HttpContextContract) {
-    const {categoriaId,id}=request.params()
-    const pdt = await Produto.query().where('id',id).andWhere('categoriaId', categoriaId).first()
+    const {categoria_id,id}=request.params()
+    const pdt = await Produto.query().where('id',id).andWhere('categoria_id', categoria_id).first()
     if (!pdt){
       return response.notFound()
     }
